721a7aa824fd9392baeee08300740d3570b5def8
Larry Morales <larrymoralesjordan@gmail.com>
2014-11-19 21:43:38 -0500

    [Feature] add support for View a hotel information

--- HotelesExpidia/Controllers/HotelController.cs
+++ HotelesExpidia/Controllers/HotelController.cs
@@ -12,14 +12,21 @@ namespace HotelesExpidia.Controllers
         public string Get()
         {
             WebClient clienteTom = new WebClient();
-            string respuesta = clienteTom.DownloadString("https://api.eancdn.com/ean-services/rs/hotel/v3/list?apiKey=cbrzfta369qwyrm9t5b8y8kf&cid=55505&minorRev=99&locale=en_US&currencyCode=USD&_type=json&destinationString=Barranquilla,Colombia&arrivalDate=12/15/2014&departureDate=12/17/2014");
+            string respuesta = clienteTom.DownloadString("http://dev.api.ean.com/ean-services/rs/hotel/v3/list?apiKey=cbrzfta369qwyrm9t5b8y8kf&cid=55505&minorRev=99&locale=en_US&currencyCode=USD&_type=json&destinationString=Barranquilla,Colombia&arrivalDate=12/15/2014&departureDate=12/17/2014");
             return respuesta;
         }
 
         public String Get(String ciudad)
         {
             WebClient clienteTom = new WebClient();
-            string respuesta = clienteTom.DownloadString("https://api.eancdn.com/ean-services/rs/hotel/v3/list?apiKey=cbrzfta369qwyrm9t5b8y8kf&cid=55505&minorRev=99&locale=en_US&currencyCode=USD&_type=json&destinationString=" + ciudad);
+            string respuesta = clienteTom.DownloadString("http://dev.api.ean.com/ean-services/rs/hotel/v3/list?apiKey=cbrzfta369qwyrm9t5b8y8kf&cid=55505&minorRev=28&locale=en_US&currencyCode=USD&_type=json&destinationString=" + ciudad);
+            return respuesta;
+        }
+
+        public string GetHotelById(string id)
+        {
+            WebClient clienteTom = new WebClient();
+            string respuesta = clienteTom.DownloadString("http://dev.api.ean.com/ean-services/rs/hotel/v3/info?apiKey=cbrzfta369qwyrm9t5b8y8kf&cid=55505&minorRev=99&locale=en_US&currencyCode=USD&_type=json&options=0&hotelId=" + id);
             return respuesta;
         }
     }
